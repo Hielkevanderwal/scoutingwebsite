fd0eb97b31117fd3d1454ef1ea104e0109a8e4b5
Hielke van der Wal <hielke010@gmail.com>
2023-06-21 16:18:31 +0200

    basic Album model

--- mediamanager/models.py
+++ mediamanager/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# todo move to organisation app
+class Speltak(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Album(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    upload_date = models.DateTimeField()
+    foto_count = models.IntegerField(default=0)
+
+    speltak = models.ManyToManyField(Speltak, blank=True)
+
+    def __str__(self):
+        return self.name
--- scoutingwebsite/settings.py
+++ scoutingwebsite/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "mediamanager",
+
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
