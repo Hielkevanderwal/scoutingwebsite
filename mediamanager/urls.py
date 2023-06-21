9729992fc0d5eb1fd8160f62eb1184397a16c0a0
Hielke van der Wal <hielke010@gmail.com>
2023-06-21 16:30:41 +0200

    first view

---
+++ mediamanager/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import UploadView
+
+urlpatterns = [
+    path("upload/", UploadView.as_view()),
+]
--- scoutingwebsite/settings.py
+++ scoutingwebsite/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "scoutingwebsite.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "mediamanager\\templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
