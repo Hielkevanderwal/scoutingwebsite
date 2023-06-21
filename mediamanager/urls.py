4915e05cb1a00c6237aacafb3cecc19ed6580a2f
Hielke van der Wal <hielke010@gmail.com>
2023-06-21 16:35:38 +0200

    added minimum pages

--- mediamanager/urls.py
+++ mediamanager/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import UploadView
+from .views import UploadView, AlbumView, AlbumOverviewView
 
 urlpatterns = [
     path("upload/", UploadView.as_view()),
+    path("", AlbumOverviewView.as_view()),
+    path("Album/", AlbumView.as_view())
 ]
--- mediamanager/views.py
+++ mediamanager/views.py
@@ -5,3 +5,9 @@ from django.views.generic import TemplateView
 # Create your views here.
 class UploadView(TemplateView):
     template_name = "upload.html"
+
+class AlbumOverviewView(TemplateView):
+    template_name = "media_index.html"
+
+class AlbumView(TemplateView):
+    template_name = "album.html"
