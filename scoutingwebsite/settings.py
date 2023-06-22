5873b6dd8ed6d75f71900e265540040abd78457f
Hielke van der Wal <hielke010@gmail.com>
2023-06-22 10:55:46 +0200

    start of the base app

--- scoutingwebsite/settings.py
+++ scoutingwebsite/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = "scoutingwebsite.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            os.path.join(BASE_DIR, "base\\templates")
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- scoutingwebsite/urls.py
+++ scoutingwebsite/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from base import urls as base_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include(base_urls.urlpatterns))
 ]
